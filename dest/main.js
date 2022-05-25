29b575f0289d957bed56d8b4dcff7595a2e4bb57
phamxuansanga11 <phamxuansangct01@gmail.com>
2022-05-25 17:09:27 +0700

    update css

--- dest/main.js
+++ dest/main.js
@@ -1,1 +1,44 @@
-console.log('Hello CFD Circle');
+//event click active menu
+const tagaHeader = document.querySelectorAll(
+  ".main__header .main__header-nav ul li a"
+);
+
+tagaHeader.forEach((taga) => {
+  taga.addEventListener("click", (e) => {
+    e.preventDefault();
+    tagaHeader.forEach((removeActive) => {
+      removeActive.classList.remove("active");
+    });
+    taga.classList.add("active");
+  });
+});
+
+//backtotop
+const btnBackToTop = document.querySelector(
+  "footer .--container__footer .backtotop"
+);
+btnBackToTop.addEventListener("click", (e) => {
+  scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", (e) => {
+  let scrollY = window.pageYOffset;
+  if (scrollY > 2000) {
+    btnBackToTop.style.display = "flex";
+  } else {
+    btnBackToTop.style.display = "none";
+  }
+});
+
+//slider
+
+// var elem = document.querySelector(".main-carousel");
+// var flkty = new Flickity(elem, {
+//   // options
+//   cellAlign: "left",
+//   contain: true,
+//   imagesLoaded: true,
+// });
